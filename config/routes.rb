6671d956c41eb84d5440024d8097aa46fbb47830
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2022-11-07 07:02:05 +0000

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update] do
